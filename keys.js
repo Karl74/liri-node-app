d9cb8628399d4d5c04f5700f77f436422b3a0d93
Carlos Castro <carlos.castro74@gmail.com>
2017-04-01 21:13:00 -0500

    all the data is loading

---
+++ keys.js
@@ -0,0 +1,14 @@
+console.log("this is loaded");
+
+exports.twitterKeys = {
+	consumer_key: "<SrTf2xShu4lxSRO3NfCQrTgnx>",
+	consumer_secret:"<HR1sO5p6uZgh68k1ZGBYbxtCTczWG5sstALMIbBhpN6cYxmFlp>",
+	access_token_key:"<3266126256-fb7v1oniriJkX1sP8QUZOml0jYO3MhhVNpxAnyR>",
+	access_token_secret: "<sBkzlp9Et7Kolz1TKO90Ma4M9Ti92kVs6PrmcQsPLoTNO>"
+	}
+
+
+exports.myTest = {
+	hello: "hi",
+	who: "me",
+}
